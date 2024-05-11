9a190723a0f8cd1819555d7e1ff46d41323e1edd
Seth Ahrenbach <seth.ahrenbach@omnifederal.com>
2024-05-11 11:58:53 -0500

    check for lack of assert/invariant

--- dafny_qa_scripts/check_trivial.py
+++ dafny_qa_scripts/check_trivial.py
@@ -7,6 +7,7 @@ compilable_directory = os.path.join(parent_directory, "compilable")
 
 # Output file path
 output_file = os.path.join(current_directory + '/trivial_files.txt')
+output_file2 = os.path.join(current_directory + '/trivial_files2.txt')
 
 # Open the output file in append mode
 with open(output_file, 'a') as file:
@@ -21,5 +22,22 @@ with open(output_file, 'a') as file:
             # Check if the file contains the word "ensures"
             if 'ensures' not in contents:
                 if 'requires' not in contents:
+                    # Write the filename to the output file
+                    file.write(filename + '\n')
+            
+# Open the output file in append mode
+with open(output_file2, 'a') as file:
+    # Iterate through the files in the directory
+    for filename in os.listdir(compilable_directory):
+        # Check if the file is a regular file
+        if os.path.isfile(os.path.join(compilable_directory, filename)):
+            # Read the contents of the file
+            with open(os.path.join(compilable_directory, filename), 'r') as f:
+                contents = f.read()
+            
+                        
+            # Check if the file contains the word "invariant" or "assert"
+            if 'invariant' not in contents:
+                if 'assert' not in contents:
                     # Write the filename to the output file
                     file.write(filename + '\n')
